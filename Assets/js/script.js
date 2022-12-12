ef1a8c6ff94181a8d5ef6c229d5ec31e5bf21bfc
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-11 20:50:09 -0500

    Psuedo code for API

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+// Grab first API to get location cordinates (lon, latitiude)
+
+// Grad second API to get weather for 5 days
+    // Date (moment.js UTC conversion)
+    // Icon
+    // Temp
+    // Wind
+    // Humidity
