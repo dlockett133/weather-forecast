62a24bb63af9941425b4054974e548c484bd26a6
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-11 22:03:45 -0500

    created API fetch for getting geo cordinates

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,8 +6,24 @@
     // Temp
     // Wind
     // Humidity
-    var dateFormat = moment().format("M/D/YYYY") // Add UTC
+var dateFormat = moment().format("M/D/YYYY") // Add UTC
 
-    const apiKey = `d08a795d9cdd7f108bc04f749cd0193c`
-    var geocodeAPI = `http://api.openweathermap.org/geo/1.0/direct?q=&appid=${apiKey}&limit=1`
-    var weatherAPI = `api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid=${apiKey}&units=imperial&cnt=7&units=imperial`
+const apiKey = `d08a795d9cdd7f108bc04f749cd0193c`
+
+function getLocation (city) {
+    var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&limit=1`
+
+    fetch (geocodeUrl)
+    .then(response => response.json())
+    .then(data => {
+        var lat = data[0].lat // retrieves latitude
+        var lon = data[0].lon // retrieves longitude
+    })
+
+    
+}
+
+function getWeather(lat, lon) {
+    var weatherUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial&cnt=7&units=imperial`
+
+}
