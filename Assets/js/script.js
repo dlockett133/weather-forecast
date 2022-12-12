c3117464deae673f3e38ca5b9ab6e0262e23fbb5
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-11 21:41:34 -0500

    Added an extra weather card, added API urls for request

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,4 +8,6 @@
     // Humidity
     var dateFormat = moment().format("M/D/YYYY") // Add UTC
 
-    
+    const apiKey = `d08a795d9cdd7f108bc04f749cd0193c`
+    var geocodeAPI = `http://api.openweathermap.org/geo/1.0/direct?q=&appid=${apiKey}&limit=1`
+    var weatherAPI = `api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid=${apiKey}&units=imperial&cnt=7&units=imperial`
